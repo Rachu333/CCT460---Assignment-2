8ac2c2a750d67e2ee9c60e76d8447cb949f64d82
Rachu333 <rachana_sai@hotmail.com>
2015-08-04 16:00:48 -0400

    Page

--- el_burrito/page.php
+++ el_burrito/page.php
@@ -12,6 +12,8 @@
 
 get_header(); ?>
 
+<?wp_nav_menu( array( 'theme_location' => 'extra-menu', 'container_class' => 'my_extra_menu_class' ) ); ?>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
