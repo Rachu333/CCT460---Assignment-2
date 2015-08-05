4f4aef7a6c2beb9a7068048a5eedeb415f016b70
Rachu333 <rachana_sai@hotmail.com>
2015-08-05 18:30:23 -0400

    Edited the arrangement; minor tweaks.

--- el_burrito/inc/options.php
+++ el_burrito/inc/options.php
@@ -5,8 +5,9 @@
 * Description: Options
 * Author: Isha Thaker
 * Version: 1.0
-* Author URL: phoenix.sheridanc.on.ca/~ccit2716
+* Author URL: phoenix.sheridanc.on.ca/~ccit2710
 */
+
 
 function cd_add_submenu(){
 	add_submenu_page( 'options-general.php', 'Submenu', 
@@ -14,6 +15,8 @@ function cd_add_submenu(){
 	'cd_display_submenu_options');
 	}
 	add_action( 'admin_menu', 'cd_add_submenu' );
+	
+	
 
 function cd_add_admin_menu(){ 
 	add_menu_page( 'My Options Page', 
@@ -23,6 +26,7 @@ function cd_add_admin_menu(){
 	add_action( 'admin_menu', 'cd_add_admin_menu' );
 	
 function cd_settings_init() { 
+	
 	register_setting( 'theme_options', 'cd_options_settings' );
 	
 	add_settings_section(
@@ -33,12 +37,13 @@ function cd_settings_init() {
 	);
 	
 function cd_options_page_section_callback() { 
-	echo __( 'A description and detail about the 
-	section.', 'codediva' );
+	echo __( 'A description and detail about the section.', 'codediva' );
 	}
+
+add_settings_field( $id, $title, $callback, $page, $section, $args);
 	
-	add_settings_field( 
-	'cd_text_field', 
+	add_settings_field(
+		'cd_text_field', 
 	__('Enter your text', 'codediva'), 
 	'cd_text_field_render', 
 	'theme_options', 
@@ -46,112 +51,93 @@ function cd_options_page_section_callback() {
 	);
 	
 	add_settings_field( 
-	'cd_checkbox_field', 
-	__( 'Check your preference', 'codediva' ), 
-	'cd_checkbox_field_render', 
-	'theme_options', 
-	'cd_options_page_section'
+		'cd_checkbox_field', 
+		__('Check your preference', 'codediva'), 
+		'cd_checkbox_field_render', 
+		'theme_options', 
+		'cd_options_page_section'
 	);
 	
 	add_settings_field( 
-	'cd_radio_field', 
-	__( 'Choose an option', 'codediva' ), 
-	'cd_radio_field_render', 
-	'theme_options', 
-	'cd_options_page_section'
+		'cd_radio_field', 
+		__('Choose an option', 'codediva'), 
+		'cd_radio_field_render', 
+		'theme_options', 
+		'cd_options_page_section'
 	);
 	
 	add_settings_field( 
-	'cd_textarea_field', 
-	__( 'Enter content in the textarea', 'codediva' ), 
-	'cd_textarea_field_render', 
-	'theme_options', 
-	'cd_options_page_section'
+		'cd_textarea_field', 
+		__('Enter content in the textarea', 'codediva'), 
+		'cd_textarea_field_render', 
+		'theme_options', 
+		'cd_options_page_section'
 	);
 	
-	add_settings_field( 
-	'cd_select_field', 
-	__( 'Choose from the dropdown', 'codediva' ), 
-	'cd_select_field_render', 
-	'theme_options', 
-	'cd_options_page_section'
+	add_settings_field( 'cd_select_field', 
+		__('Choose from the dropdown', 'codediva'), 
+		'cd_select_field_render', 
+		'theme_options', 
+		'cd_options_page_section'
 	);
 	
 function cd_text_field_render() { 
-	$options = get_option( 'cd_options_settings' );
+	$options = get_option('cd_options_settings');
 	?>
-	<input type="text"name="cd_options_settings
-	[cd_text_field]"value="<?php if (isset($options['cd_text_field'])) 
+	<input type="text" name="cd_options_settings
+	[cd_text_field]" value="<?php if (isset($options['cd_text_field'])) 
 	echo $options['cd_text_field']; ?>">
 	<?php
 	}
 	
 function cd_checkbox_field_render() { 
-	$options=get_option( 'cd_options_settings' );
+	$options=get_option('cd_options_settings');
 	?>
-	<input type="checkbox"name="cd_options_settings
-	[cd_checkbox_field]" <?php if (isset($options
-	['cd_checkbox_field'])) checked( $options['cd_checkbox_field'], 1
-	); ?> value="1">
+	<input type="checkbox" name="cd_options_settings
+	[cd_checkbox_field]" <?php if (isset($options['cd_checkbox_field'])) checked( $options['cd_checkbox_field'], 1); ?> value="1">
 	<?php
 	}	
 
 function cd_radio_field_render() { 
-$options = get_option( 'cd_options_settings' );
-?>
-<input type="radio" name="cd_options_settings
-[cd_radio_field]" <?php	if (isset($options['cd_radio_field'])) 
-checked( $options['cd_radio_field'], 1 ); ?> value="1">
-<?php
+	$options = get_option('cd_options_settings');
+	?>
+		<input type="radio" name="cd_options_settings
+	[cd_radio_field]" <?php	if (isset($options['cd_radio_field'])) checked( $options['cd_radio_field'], 1 ); ?> value="1">
+	<?php
 }
 
 function cd_textarea_field_render() { 
-$options = get_option( 'cd_options_settings' );
-?>
-	<textarea cols="40"rows="5"name="cd_options_settings
-[cd_textarea_field]">
-<?php if (isset($options['cd_textarea_field'])) echo$options
-['cd_textarea_field']; ?>
-</textarea>
-<?php
-}
+	$options = get_option('cd_options_settings');
+	?>
+	<textarea cols="40" rows="5" name="cd_options_settings[cd_textarea_field]">
+		<?php if (isset($options['cd_textarea_field'])) echo$options['cd_textarea_field']; ?>
+	</textarea>
+	<?php
+	}
 
 function cd_select_field_render() { 
-$options = get_option( 'cd_options_settings' );
+$options = get_option('cd_options_settings');
 ?>
-<select name="cd_options_settings[cd_select_field]">
-	<option value="1" <?php	if (isset($options['cd_select_field'])) 
-selected( $options['cd_select_field'], 1 ); ?>>Option 1</option>
-	<option value="2" <?php	if (isset($options['cd_select_field'])) 
-selected( $options['cd_select_field'], 2 ); ?>>Option 2</option></select>
+	<select name="cd_options_settings[cd_select_field]">
+		<option value="1" <?php	if (isset($options['cd_select_field'])) selected( $options['cd_select_field'], 1 ); ?>>Option 1</option>
+		<option value="2" <?php	if (isset($options['cd_select_field'])) selected( $options['cd_select_field'], 2 ); ?>>Option 2</option>
+	</select>
 <?php
-}
 }
 
 function my_theme_options_page(){ 
 	?>
-	<form action="options.php"method="post">
+	<form action="options.php" method="post">
 		<h2>My Awesome Options Page</h2>
 	<?php
 	settings_fields( 'theme_options' );
 	do_settings_sections( 'theme_options' );
 	submit_button();
 	?>
-</form>
-
-<?php
-$text = get_option(‘cd_options_settings’);
-echo $text[‘cd_text_field’];
-echo '<br/>';
-echo $text[‘cd_checkbox_field’];
-echo '<br />';
-echo $text[‘cd_radio_field’];
-echo '<br />';
-echo $text[‘cd_select_field’];
-echo '<br />';
-echo $text[‘cd_textarea_field’];
-}
-?>
+	</form>
 	<?php
-	add_action( 'admin_init', 'cd_settings_init' );	
-?>
+
+	}
+}
+
+add_action( 'admin_init', 'cd_settings_init' );	
