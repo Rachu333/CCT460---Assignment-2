4a1c84e0a1d94640a0f6f8454ad762c65e8439e8
Rachu333 <rachana_sai@hotmail.com>
2015-08-04 23:50:18 -0400

    Didn't change anything!

--- el_burrito/page.php
+++ el_burrito/page.php
@@ -17,7 +17,8 @@ wp_nav_menu( array( 'el_burrito' => 'extra-menu', 'container_class' => 'my_extra
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
+			
+			
 			<?php while ( have_posts() ) : the_post(); ?>
 
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
